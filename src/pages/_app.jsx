29b43677786cfb23bd8776006d4e6d9577b95230
Ben Silverman <bsilverm@mit.edu>
2020-06-23 15:33:06 -0400

    #1 Eslint config

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/jsx-props-no-spreading */
+/* eslint react/prop-types: 0 */
 
 import '../style/index.css';
 
-// eslint-disable-next-line react/prop-types
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
