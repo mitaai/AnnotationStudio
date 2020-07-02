1f35c21f5c620a0013b00821370bc31a39bf58e2
Ben Silverman <bsilverm@mit.edu>
2020-07-02 15:19:57 -0400

    #6 add footer links and moment.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,41 @@
 import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 import Image from 'react-bootstrap/Image';
+import Link from 'next/link';
+import moment from 'moment';
 
 function Footer() {
   return (
     <footer className="footer mt-auto py-3">
-      <Container>
-        <Image src="/logo_mit.png" alt="MIT logo" className="logo mr-3" />
-        <Image src="/logo_aai.png" alt="Active Archives Initiative logo" className="logo mr-3" />
-        <Image src="/logo_neh.png" alt="National Endowment for the Humanities logo" className="logo" />
+      <Container className="px-0">
+        <Row>
+          <Col>
+            <Image src="/logo_mit.png" alt="MIT logo" className="logo mr-3 mt-2" />
+            <Image src="/logo_aai.png" alt="Active Archives Initiative logo" className="logo mr-3 mt-2" />
+            <Image src="/logo_neh.png" alt="National Endowment for the Humanities logo" className="logo mt-2" />
+          </Col>
+          <Col className="text-muted text-right mt-n1">
+            <small>
+              &copy;
+              {' '}
+              {moment().year()}
+              {' '}
+              Active Archives Initiative at MIT
+              <br />
+              <Link href="#tos">Terms and Conditions</Link>
+              {' '}
+              |
+              {' '}
+              <Link href="#privacy">Privacy Policy</Link>
+              {' '}
+              |
+              {' '}
+              <Link href="#cookie">Cookie Policy</Link>
+              {' '}
+            </small>
+          </Col>
+        </Row>
       </Container>
       <style jsx>
         {`
