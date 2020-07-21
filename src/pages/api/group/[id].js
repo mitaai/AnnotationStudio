f5899e9340825a0671b9dee1c8ac898ae611e06e
Ben Silverman <bsilverm@mit.edu>
2020-07-20 21:08:59 -0400

    #26 Add Group PATCH

--- src/pages/api/group/[id].js
+++ src/pages/api/group/[id].js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import nc from 'next-connect';
 import { ObjectID } from 'mongodb';
 import middleware from '../../../middlewares/middleware';
@@ -32,6 +33,38 @@ const handler = nc()
             }
           },
         );
+  .patch(
+    async (req, res) => {
+      const token = await jwt.getJwt({ req, secret });
+      if (token && token.exp > 0) {
+        const memberQuery = (process.env.NODE_ENV === 'development') ? [{ members: ObjectID(token.user.id) }, { members: getObjectId('FakeUserReplaceMe') }] : [{ members: ObjectID(token.user.id) }];
+        const fieldsToSet = req.body.name ? { name: req.body.name } : {};
+        const membersToPush = req.body.addedUserId ? { members: ObjectID(req.body.addedUserId) } : {};
+        const membersToPull = req.body.removedUserId ? { members: ObjectID(req.body.removedUserId) } : {};
+        await req.db
+          .collection('groups')
+          .findOneAndUpdate(
+            {
+              _id: ObjectID(req.query.id),
+              $or: memberQuery,
+            },
+            {
+              $set: fieldsToSet,
+              $push: membersToPush,
+              $pull: membersToPull,
+              $currentDate: {
+                updatedAt: true,
+              },
+            },
+            {
+              returnOriginal: false,
+            },
+            (err, doc) => {
+              if (err) throw err;
+              res.status(200).json(doc);
+            },
+          );
+      } else res.status(403).json({ error: '403 Invalid or expired token' });
     },
   );
 
