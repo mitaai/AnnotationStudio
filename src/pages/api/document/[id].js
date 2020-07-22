09c343ea394215dfabf1445bd3d5c428ef95c93b
Ben Silverman <bsilverm@mit.edu>
2020-07-22 17:54:38 -0400

    #26 Add Document PATCH route

--- src/pages/api/document/[id].js
+++ src/pages/api/document/[id].js
@@ -86,6 +86,112 @@ const handler = nc()
           );
       } else res.status(403).json({ error: '403 Invalid or expired token' });
     },
+  )
+  .patch(
+    async (req, res) => {
+      const token = await jwt.getJwt({ req, secret });
+      if (token && token.exp > 0) {
+        const {
+          title,
+          slug,
+          resourceType,
+          authors,
+          publisher,
+          publicationDate,
+          bookTitle,
+          edition,
+          url,
+          accessed,
+          rightsStatus,
+          location,
+          state,
+          text,
+          editors,
+          volume,
+          issue,
+          pageNumbers,
+          publication,
+          series,
+          sesiesNumber,
+          notes,
+        } = req.body;
+        const fieldsToSet = {
+          title,
+          slug,
+          resourceType,
+          authors,
+          publisher,
+          publicationDate,
+          bookTitle,
+          edition,
+          url,
+          accessed,
+          rightsStatus,
+          location,
+          state,
+          text,
+          editors,
+          volume,
+          issue,
+          pageNumbers,
+          publication,
+          series,
+          sesiesNumber,
+          notes,
+        };
+        Object.keys(fieldsToSet).forEach((key) => {
+          if (fieldsToSet[key] === undefined) {
+            delete fieldsToSet[key];
+          }
+        });
+        const groupToPush = req.body.addedGroup
+          ? { groups: req.body.addedGroup }
+          : {};
+        const groupToPull = req.body.removedGroupId
+          ? { 'groups.id': ObjectID(req.body.removedGroupId) }
+          : {};
+        let groupById = {};
+        let groupFieldsToSet = {};
+        let memberToPush = {};
+        let memberToPull = {};
+        if (req.body.updatedGroup) {
+          groupById = { 'groups.id': ObjectID(req.body.updatedGroup.id) };
+          if (req.body.updatedGroup.name) {
+            groupFieldsToSet = { 'groups.$.name': req.body.updatedGroup.name };
+          }
+          if (req.body.addedUser) {
+            memberToPush = { 'groups.$.members': req.body.addedUser };
+          }
+          if (req.body.removedUserId) {
+            memberToPull = { 'groups.$.members.id': ObjectID(req.body.removedUserId) };
+          }
+        }
+        await req.db
+          .collection('documents')
+          .findOneAndUpdate(
+            {
+              _id: ObjectID(req.query.id),
+              owner: ObjectID(token.user.id),
+              ...groupById,
+            },
+            {
+              $set: { ...fieldsToSet, ...groupFieldsToSet },
+              $push: { ...memberToPush, ...groupToPush },
+              $pull: { ...memberToPull, ...groupToPull },
+              $currentDate: {
+                updatedAt: true,
+              },
+            },
+            {
+              returnOriginal: false,
+            },
+            (err, doc) => {
+              if (err) throw err;
+              res.status(200).json(doc);
+            },
+          );
+      } else res.status(403).json({ error: '403 Invalid or expired token' });
+    },
   );
 
 export default handler;
