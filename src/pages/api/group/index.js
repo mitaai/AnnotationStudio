6424a18ee5771314327ed6753d69d1814350e599
Ben Silverman <bsilverm@mit.edu>
2020-07-22 12:04:02 -0400

    #26 Add Group POST route

---
+++ src/pages/api/group/index.js
@@ -0,0 +1,35 @@
+import nc from 'next-connect';
+import { ObjectID } from 'mongodb';
+import jwt from 'next-auth/jwt';
+import middleware from '../../../middlewares/middleware';
+
+const secret = process.env.AUTH_SECRET;
+
+const handler = nc()
+  .use(middleware)
+  .post(
+    async (req, res) => {
+      const token = await jwt.getJwt({ req, secret });
+      if (token && token.exp > 0) {
+        const { name } = req.body;
+        const members = [{
+          id: ObjectID(token.user.id),
+          name: token.user.name,
+          email: token.user.email,
+          role: 'owner',
+        }];
+        const documents = [{}];
+        await req.db
+          .collection('groups')
+          .insertOne(
+            { name, members, documents },
+            (err, doc) => {
+              if (err) throw err;
+              res.status(200).json(doc);
+            },
+          );
+      } else res.status(403).json({ error: '403 Invalid or expired token' });
+    },
+  );
+
+export default handler;
