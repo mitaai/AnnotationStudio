c2de7b764a058a129fddade6a524beee89d16b8a
Ben Silverman <bsilverm@mit.edu>
2020-07-28 18:38:03 -0400

    #28 Add user PATCH API route

--- src/pages/api/user/[id].js
+++ src/pages/api/user/[id].js
@@ -1,6 +1,9 @@
 import nc from 'next-connect';
+import jwt from 'next-auth/jwt';
 import { ObjectID } from 'mongodb';
 import middleware from '../../../middlewares/middleware';
+
+const secret = process.env.AUTH_SECRET;
 
 const handler = nc()
   .use(middleware)
@@ -24,6 +27,34 @@ const handler = nc()
             }
           },
         );
+    },
+  )
+  .patch(
+    async (req, res) => {
+      const token = await jwt.getJwt({ req, secret });
+      if (token && token.exp > 0) {
+        const groupToPush = req.body.addedGroup
+          ? { groups: req.body.addedGroup }
+          : {};
+        await req.db
+          .collection('users')
+          .findOneAndUpdate(
+            { _id: ObjectID(req.query.id) },
+            {
+              $push: { ...groupToPush },
+              $currentDate: {
+                updatedAt: true,
+              },
+            },
+            {
+              returnOriginal: false,
+            },
+            (err, doc) => {
+              if (err) throw err;
+              res.status(200).json(doc);
+            },
+          );
+      } else res.status(403).json({ error: '403 Invalid or expired token' });
     },
   );
 
