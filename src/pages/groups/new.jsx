8ba1c26334bc7c9c330f34f748f07d1a2268cbf3
Ben Silverman <bsilverm@mit.edu>
2020-08-05 15:45:55 -0400

    #28 Use groupUtil function for AddGroupToUser

--- src/pages/groups/new.jsx
+++ src/pages/groups/new.jsx
@@ -1,45 +1,18 @@
 import fetch from 'unfetch';
 import { Formik } from 'formik';
-import { useSession, getSession } from 'next-auth/client';
+import { useSession } from 'next-auth/client';
 import {
   Button, Card, Col, Form, Row,
 } from 'react-bootstrap';
-import Router from 'next/router';
 import * as yup from 'yup';
+import { AddGroupToUser } from '../../utils/groupUtil';
 import Layout from '../../components/Layout';
 import LoadingSpinner from '../../components/LoadingSpinner';
 
 const NewGroup = () => {
   const [session] = useSession();
 
-  const updateOwnerWithGroup = async ({ id, name, user }) => {
-    const url = `/api/user/${user}`;
-    const body = {
-      addedGroup: {
-        id,
-        name,
-        role: 'owner',
-      },
-    };
-    const res = await fetch(url, {
-      method: 'PATCH',
-      body: JSON.stringify(body),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    if (res.status === 200) {
-      const result = await res.json();
-      getSession();
-      Router.push({
-        pathname: '/groups',
-      });
-      return Promise.resolve(result);
-    }
-    return Promise.reject(Error(`Unable to add group to user: error ${res.status} received from server`));
-  };
-
-  const submitHandler = async (values) => {
+  const createGroup = async (values) => {
     const url = '/api/group';
     const { name } = values;
     const res = await fetch(url, {
@@ -54,9 +27,14 @@ const NewGroup = () => {
       const group = {
         id: result.insertedId,
         name,
-        user: result.ops[0].members[0].id,
+        ownerName: result.ops[0].members[0].name,
+        memberCount: 1,
       };
-      return updateOwnerWithGroup(group);
+      const user = {
+        userId: result.ops[0].members[0].id,
+        role: 'owner',
+      };
+      return AddGroupToUser(group, user);
     }
     return Promise.reject(Error(`Unable to create group: error ${res.status} received from server`));
   };
@@ -79,7 +57,7 @@ const NewGroup = () => {
               <Formik
                 onSubmit={(values, actions) => {
                   setTimeout(() => {
-                    submitHandler(values);
+                    createGroup(values);
                     actions.setSubmitting(false);
                   }, 1000);
                 }}
