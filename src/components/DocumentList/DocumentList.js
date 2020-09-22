d9e8c9797bc2378f8ef835a10da45902e17390b5
Ben Silverman <bsilverm@mit.edu>
2020-09-22 16:35:25 -0600

    #41 Better loading indicators

--- src/components/DocumentList/DocumentList.js
+++ src/components/DocumentList/DocumentList.js
@@ -1,17 +1,26 @@
 import {
   Card, Table,
 } from 'react-bootstrap';
+import LoadingSpinner from '../LoadingSpinner';
 
 const DocumentList = ({
   documents,
+  loading,
 }) => (
+
   <Card>
     <Card.Body>
-      <Table>
-        {documents.map((document) => (
-          <>{JSON.stringify(document)}</>
-        ))}
-      </Table>
+      {loading && (
+        <LoadingSpinner />
+      )}
+      {!loading && (
+        <Table>
+          {documents.map((document) => (
+            // eslint-disable-next-line no-underscore-dangle
+            <tr key={document._id}>{JSON.stringify(document)}</tr>
+          ))}
+        </Table>
+      )}
     </Card.Body>
   </Card>
 );
--- src/pages/documents/index.jsx
+++ src/pages/documents/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useSession } from 'next-auth/client';
-import { Tabs, Tab } from 'react-bootstrap';
+import { Card, Tabs, Tab } from 'react-bootstrap';
 import Layout from '../../components/Layout';
 import DocumentList from '../../components/DocumentList';
 import LoadingSpinner from '../../components/LoadingSpinner';
@@ -9,6 +9,7 @@ import { getDocumentsByGroup, getDocumentsByUser } from '../../utils/docUtil';
 const DocumentsIndex = () => {
   const [session, loading] = useSession();
   const [key, setKey] = useState('shared');
+  const [listLoading, setListLoading] = useState(true);
   const [documents, setDocuments] = useState([]);
   const [alerts, setAlerts] = useState([]);
 
@@ -18,11 +19,13 @@ const DocumentsIndex = () => {
         if (key === 'shared') {
           setDocuments(
             await getDocumentsByGroup(session.user.groups)
+              .then(setListLoading(false))
               .catch((err) => setAlerts([{ text: err.message, variant: 'danger' }])),
           );
         } else if (key === 'mine') {
           setDocuments(
             await getDocumentsByUser(session.user.id)
+              .then(setListLoading(false))
               .catch((err) => setAlerts([{ text: err.message, variant: 'danger' }])),
           );
         }
@@ -34,26 +37,26 @@ const DocumentsIndex = () => {
   return (
     <Layout alerts={alerts}>
       {loading && !session && (
-        <>
+      <Card>
+        <Card.Body>
           <LoadingSpinner />
-        </>
+        </Card.Body>
+      </Card>
       )}
       {!loading && session && (
-        <>
-          <Tabs
-            transition={false}
-            id="document-list-tabs"
-            activeKey={key}
-            onSelect={(k) => setKey(k)}
-          >
-            <Tab eventKey="shared" title="Shared">
-              <DocumentList documents={documents} />
-            </Tab>
-            <Tab eventKey="mine" title="Mine">
-              <DocumentList documents={documents} />
-            </Tab>
-          </Tabs>
-        </>
+      <Tabs
+        transition={false}
+        id="document-list-tabs"
+        activeKey={key}
+        onSelect={(k) => setKey(k)}
+      >
+        <Tab eventKey="shared" title="Shared">
+          <DocumentList documents={documents} loading={listLoading} />
+        </Tab>
+        <Tab eventKey="mine" title="Mine">
+          <DocumentList documents={documents} loading={listLoading} />
+        </Tab>
+      </Tabs>
       )}
     </Layout>
   );
