aeb41eb9d1fea30b62865fed49f7e9439667c81a
Ben Silverman <bsilverm@mit.edu>
2020-10-05 16:44:03 -0600

    #48 Rewrite document api routes to use cached db

--- src/pages/api/document/[id].js
+++ src/pages/api/document/[id].js
@@ -1,106 +1,35 @@
-import nc from 'next-connect';
 import { ObjectID } from 'mongodb';
 import jwt from 'next-auth/jwt';
-import middleware from '../../../middlewares/middleware';
+import { connectToDatabase } from '../../../utils/dbUtil';
 
 const secret = process.env.AUTH_SECRET;
 
-const handler = nc()
-  .use(middleware)
-  .get(
-    async (req, res) => {
-      const token = await jwt.getToken({ req, secret });
-      if (token && token.exp > 0) {
-        const userObj = await req.db
-          .collection('users')
-          .findOne({ _id: ObjectID(token.id) });
-        const userGroups = (userObj.groups && userObj.groups.length > 0)
-          ? userObj.groups.map((group) => group.id)
-          : [];
-        await req.db
-          .collection('documents')
-          .findOne(
-            {
-              _id: ObjectID(req.query.id),
-              $or: [{ groups: { $in: userGroups } }, { owner: token.id }],
-            },
-            (err, doc) => {
-              if (doc) {
-                const {
-                  title,
-                  slug,
-                  owner,
-                  groups,
-                  resourceType,
-                  authors,
-                  publisher,
-                  publicationDate,
-                  bookTitle,
-                  edition,
-                  url,
-                  accessed,
-                  rightsStatus,
-                  location,
-                  state,
-                  text,
-                  createdAt,
-                  updatedAt,
-                  uploadContentType,
-                  editors,
-                  volume,
-                  issue,
-                  pageNumbers,
-                  publication,
-                  series,
-                  sesiesNumber,
-                  notes,
-                } = doc;
-                if (err) throw err;
-                res.status(200).json({
-                  title,
-                  slug,
-                  owner,
-                  groups,
-                  resourceType,
-                  authors,
-                  publisher,
-                  publicationDate,
-                  bookTitle,
-                  edition,
-                  url,
-                  accessed,
-                  rightsStatus,
-                  location,
-                  state,
-                  text,
-                  createdAt,
-                  updatedAt,
-                  uploadContentType,
-                  editors,
-                  volume,
-                  issue,
-                  pageNumbers,
-                  publication,
-                  series,
-                  sesiesNumber,
-                  notes,
-                });
-              } else {
-                res.status(404).json({ error: '404 Not Found' });
-              }
-            },
-          );
-      } else res.status(403).json({ error: '403 Invalid or expired token' });
-    },
-  )
-  .patch(
-    async (req, res) => {
-      const token = await jwt.getToken({ req, secret });
-      if (token && token.exp > 0) {
+const handler = async (req, res) => {
+  const { method } = req;
+  if (method === 'GET') {
+    const token = await jwt.getToken({ req, secret });
+    if (token && token.exp > 0) {
+      const { db } = await connectToDatabase();
+      const userObj = await db
+        .collection('users')
+        .findOne({ _id: ObjectID(token.id) });
+      const userGroups = (userObj.groups && userObj.groups.length > 0)
+        ? userObj.groups.map((group) => group.id)
+        : [];
+      const doc = await db
+        .collection('documents')
+        .find({
+          _id: ObjectID(req.query.id),
+          $or: [{ groups: { $in: userGroups } }, { owner: token.id }],
+        })
+        .toArray();
+      if (doc[0]) {
+        const document = doc[0];
         const {
           title,
-          groups,
           slug,
+          owner,
+          groups,
           resourceType,
           authors,
           publisher,
@@ -113,6 +42,9 @@ const handler = nc()
           location,
           state,
           text,
+          createdAt,
+          updatedAt,
+          uploadContentType,
           editors,
           volume,
           issue,
@@ -121,11 +53,12 @@ const handler = nc()
           series,
           sesiesNumber,
           notes,
-        } = req.body;
-        const fieldsToSet = {
+        } = document;
+        res.status(200).json({
           title,
-          groups,
           slug,
+          owner,
+          groups,
           resourceType,
           authors,
           publisher,
@@ -138,6 +71,9 @@ const handler = nc()
           location,
           state,
           text,
+          createdAt,
+          updatedAt,
+          uploadContentType,
           editors,
           volume,
           issue,
@@ -146,69 +82,117 @@ const handler = nc()
           series,
           sesiesNumber,
           notes,
-        };
-        Object.keys(fieldsToSet).forEach((key) => {
-          if (fieldsToSet[key] === undefined) {
-            delete fieldsToSet[key];
-          }
         });
-        const updateMethods = {};
-        let groupById = {};
-
-        if (req.body.removedGroupId) {
-          groupById = { groups: req.body.removedGroupId };
-          const groupToPull = { groups: req.body.removedGroupId };
-          const fieldsToPull = { ...groupToPull };
-          if (Object.keys(fieldsToPull).length !== 0) updateMethods.$pull = fieldsToPull;
+      } else res.status(404).end('404 Not Found');
+    } else res.status(403).end('403 Invalid or expired token');
+  } else if (method === 'PATCH') {
+    const token = await jwt.getToken({ req, secret });
+    if (token && token.exp > 0) {
+      const {
+        title,
+        groups,
+        slug,
+        resourceType,
+        authors,
+        publisher,
+        publicationDate,
+        bookTitle,
+        edition,
+        url,
+        accessed,
+        rightsStatus,
+        location,
+        state,
+        text,
+        editors,
+        volume,
+        issue,
+        pageNumbers,
+        publication,
+        series,
+        sesiesNumber,
+        notes,
+      } = req.body;
+      const fieldsToSet = {
+        title,
+        groups,
+        slug,
+        resourceType,
+        authors,
+        publisher,
+        publicationDate,
+        bookTitle,
+        edition,
+        url,
+        accessed,
+        rightsStatus,
+        location,
+        state,
+        text,
+        editors,
+        volume,
+        issue,
+        pageNumbers,
+        publication,
+        series,
+        sesiesNumber,
+        notes,
+      };
+      Object.keys(fieldsToSet).forEach((key) => {
+        if (fieldsToSet[key] === undefined) {
+          delete fieldsToSet[key];
         }
+      });
+      const updateMethods = {};
+      let groupById = {};
 
-        const groupToPush = req.body.addedGroup
-          ? { groups: req.body.addedGroup }
-          : {};
-        const fieldsToPush = { ...groupToPush };
-        if (Object.keys(fieldsToPush).length !== 0) updateMethods.$push = fieldsToPush;
+      if (req.body.removedGroupId) {
+        groupById = { groups: req.body.removedGroupId };
+        const groupToPull = { groups: req.body.removedGroupId };
+        const fieldsToPull = { ...groupToPull };
+        if (Object.keys(fieldsToPull).length !== 0) updateMethods.$pull = fieldsToPull;
+      }
 
-        if (Object.keys(fieldsToSet).length !== 0) updateMethods.$set = fieldsToSet;
-        updateMethods.$currentDate = { updatedAt: true };
+      const groupToPush = req.body.addedGroup
+        ? { groups: req.body.addedGroup }
+        : {};
+      const fieldsToPush = { ...groupToPush };
+      if (Object.keys(fieldsToPush).length !== 0) updateMethods.$push = fieldsToPush;
 
-        await req.db
-          .collection('documents')
-          .findOneAndUpdate(
-            {
-              _id: ObjectID(req.query.id),
-              owner: token.id,
-              ...groupById,
-            },
-            updateMethods,
-            {
-              returnOriginal: false,
-            },
-            (err, doc) => {
-              if (err) throw err;
-              res.status(200).json(doc);
-            },
-          );
-      } else res.status(403).json({ error: '403 Invalid or expired token' });
-    },
-  )
-  .delete(
-    async (req, res) => {
-      const token = await jwt.getToken({ req, secret });
-      if (token && token.exp > 0) {
-        await req.db
-          .collection('documents')
-          .findOneAndDelete(
-            {
-              _id: ObjectID(req.query.id),
-              owner: token.id,
-            },
-            (err, doc) => {
-              if (err) throw err;
-              res.status(200).json(doc);
-            },
-          );
-      } else res.status(403).json({ error: '403 Invalid or expired token' });
-    },
-  );
+      if (Object.keys(fieldsToSet).length !== 0) updateMethods.$set = fieldsToSet;
+      updateMethods.$currentDate = { updatedAt: true };
+
+      const { db } = await connectToDatabase();
+      const doc = await db
+        .collection('documents')
+        .findOneAndUpdate(
+          {
+            _id: ObjectID(req.query.id),
+            owner: token.id,
+            ...groupById,
+          },
+          updateMethods,
+          {
+            returnOriginal: false,
+          },
+        );
+      res.status(200).json(doc);
+    } else res.status(403).json({ error: '403 Invalid or expired token' });
+  } else if (method === 'DELETE') {
+    const token = await jwt.getToken({ req, secret });
+    if (token && token.exp > 0) {
+      const { db } = await connectToDatabase();
+      const doc = await db
+        .collection('documents')
+        .findOneAndDelete(
+          {
+            _id: ObjectID(req.query.id),
+            owner: token.id,
+          },
+        );
+      res.status(200).json(doc);
+    } else res.status(403).json({ error: '403 Invalid or expired token' });
+  } else res.status(405).end(`Method ${method} Not Allowed`);
+};
 
 export default handler;
--- src/pages/api/documents.js
+++ src/pages/api/documents.js
@@ -1,26 +1,27 @@
 import jwt from 'next-auth/jwt';
-import nc from 'next-connect';
-import middleware from '../../middlewares/middleware';
+import { connectToDatabase } from '../../utils/dbUtil';
 
 const secret = process.env.AUTH_SECRET;
 
-const handler = nc()
-  .use(middleware)
-  .post(async (req, res) => {
+const handler = async (req, res) => {
+  const { method } = req;
+  if (method === 'POST') {
     const token = await jwt.getToken({ req, secret });
     if (token && token.exp > 0) {
       if (req.body.userId || req.body.groupIds) {
+        const { db } = await connectToDatabase();
         const condition = req.body.userId
           ? { owner: req.body.userId }
           : { groups: { $in: req.body.groupIds } };
-        const arr = await req.db
+        const arr = await db
           .collection('documents')
           .find(condition)
           .toArray();
         if (arr.length > 0) res.status(200).json({ documents: arr });
-        else res.status(404).json({ error: '404 File Not Found' });
-      } else res.status(400).json({ error: '400 Bad request' });
-    } else res.status(403).json({ error: '403 Invalid or expired token' });
-  });
+        else res.status(404).end('File Not Found');
+      } else res.status(400).end('Bad request');
+    } else res.status(403).end('Invalid or expired token');
+  } else res.status(405).end(`Method ${method} Not Allowed`);
+};
 
 export default handler;
