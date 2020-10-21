b91a5ce404ac9d1de6938ea7139804cebb54c3d4
Ben Silverman <bsilverm@mit.edu>
2020-10-21 17:02:56 -0600

    #50 Allow admin to modify user profile

--- src/components/Admin/User/AdminUserTable/AdminUserTable.js
+++ src/components/Admin/User/AdminUserTable/AdminUserTable.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useState } from 'react';
 import {
-  Badge, Button, Table,
+  Badge, Button, Dropdown, DropdownButton, Table,
 } from 'react-bootstrap';
 import { format } from 'date-fns';
 import AdminRoleBadge from '../../AdminRoleBadge';
@@ -30,135 +30,158 @@ const AdminUserTable = ({ user, alerts, setAlerts }) => {
   };
 
   return (
-    <Table
-      striped
-      bordered
-      hover
-      size="sm"
-      variant="light"
-      style={{ borderCollapse: 'unset' }}
-    >
-      <thead>
-        <tr>
-          <td colSpan="2" className="text-center">View User</td>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th style={{ width: '25%' }}>First Name</th>
-          <td>{user.firstName}</td>
-        </tr>
-        <tr>
-          <th>Last Name</th>
-          <td>{user.lastName}</td>
-        </tr>
-        <tr>
-          <th>Email</th>
-          <td>{user.email}</td>
-        </tr>
-        <tr>
-          <th>Slug</th>
-          <td>{user.slug}</td>
-        </tr>
-        <tr>
-          <th>Affiliation</th>
-          <td>{user.affiliation}</td>
-        </tr>
-        <tr>
-          <th>Role</th>
-          <td><AdminRoleBadge role={user.role} /></td>
-        </tr>
-        <tr>
-          <th>Registered</th>
-          <td>{format(new Date(user.createdAt), 'PPPppp')}</td>
-        </tr>
-        <tr>
-          <th>Updated</th>
-          <td>{format(new Date(user.updatedAt), 'PPPppp')}</td>
-        </tr>
-        <tr>
-          <th>Last login</th>
-          <td>{user.emailVerified && format(new Date(user.emailVerified), 'PPPppp')}</td>
-        </tr>
-        <tr>
-          <th>Groups</th>
-          <td>
-            {(user.groups && user.groups.length > 0)
-              ? user.groups.sort().map((group) => {
-                let variant;
-                switch (group.role) {
-                  case 'member':
-                    variant = 'secondary';
-                    break;
-                  case 'manager':
-                    variant = 'warning';
-                    break;
-                  case 'owner':
-                    variant = 'primary';
-                    break;
-                  default:
-                    variant = 'secondary';
-                    break;
-                }
-                return (
-                  <Badge
-                    variant={variant}
-                    className="mr-1"
-                    as={Button}
-                    href={`/admin/group/${group.id}`}
-                    key={group.id}
-                  >
-                    {group.name}
-                  </Badge>
-                );
-              })
-              : (<Badge>[no groups]</Badge>)}
-          </td>
-        </tr>
-        <tr>
-          <th>Documents</th>
-          <td>
-            {docs.found && docs.found.length > 0 && (
-              docs.found.map((doc) => (
-                <Button key={doc._id} variant="link" size="sm" href={`/admin/document/${doc.slug}`}>
-                  {doc.title}
+    <>
+      <Table
+        striped
+        bordered
+        hover
+        size="sm"
+        variant="light"
+        style={{ borderCollapse: 'unset' }}
+      >
+        <thead>
+          <tr>
+            <td colSpan="2" className="text-center">View User</td>
+            <div style={{ position: 'absolute', right: '2em' }} id="user-dropdown">
+              <DropdownButton
+                size="sm"
+                variant="text"
+                drop="down"
+                menuAlign="right"
+                title="Actions"
+              >
+                <Dropdown.Item eventKey="1" href={`/user/${user.slug}/editprofile`}>Modify user</Dropdown.Item>
+                <Dropdown.Item eventKey="2" disabled={user.role === 'admin'}>Promote to admin</Dropdown.Item>
+                <Dropdown.Item eventKey="3">Delete user</Dropdown.Item>
+              </DropdownButton>
+            </div>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th style={{ width: '25%' }}>First Name</th>
+            <td>{user.firstName}</td>
+          </tr>
+          <tr>
+            <th>Last Name</th>
+            <td>{user.lastName}</td>
+          </tr>
+          <tr>
+            <th>Email</th>
+            <td>{user.email}</td>
+          </tr>
+          <tr>
+            <th>Slug</th>
+            <td>{user.slug}</td>
+          </tr>
+          <tr>
+            <th>Affiliation</th>
+            <td>{user.affiliation}</td>
+          </tr>
+          <tr>
+            <th>Role</th>
+            <td><AdminRoleBadge role={user.role} /></td>
+          </tr>
+          <tr>
+            <th>Registered</th>
+            <td>{format(new Date(user.createdAt), 'PPPppp')}</td>
+          </tr>
+          <tr>
+            <th>Updated</th>
+            <td>{format(new Date(user.updatedAt), 'PPPppp')}</td>
+          </tr>
+          <tr>
+            <th>Last login</th>
+            <td>{user.emailVerified && format(new Date(user.emailVerified), 'PPPppp')}</td>
+          </tr>
+          <tr>
+            <th>Groups</th>
+            <td>
+              {(user.groups && user.groups.length > 0)
+                ? user.groups.sort().map((group) => {
+                  let variant;
+                  switch (group.role) {
+                    case 'member':
+                      variant = 'secondary';
+                      break;
+                    case 'manager':
+                      variant = 'warning';
+                      break;
+                    case 'owner':
+                      variant = 'primary';
+                      break;
+                    default:
+                      variant = 'secondary';
+                      break;
+                  }
+                  return (
+                    <Badge
+                      variant={variant}
+                      className="mr-1"
+                      as={Button}
+                      href={`/admin/group/${group.id}`}
+                      key={group.id}
+                    >
+                      {group.name}
+                    </Badge>
+                  );
+                })
+                : (<Badge>[no groups]</Badge>)}
+            </td>
+          </tr>
+          <tr>
+            <th>Documents</th>
+            <td>
+              {docs.found && docs.found.length > 0 && (
+                docs.found.map((doc) => (
+                  <Button key={doc._id} variant="link" size="sm" href={`/admin/document/${doc.slug}`}>
+                    {doc.title}
+                  </Button>
+                ))
+              )}
+              {docs.found && docs.found.length === 0 && (
+                <Button variant="text" size="sm" disabled>
+                  This user has not created any documents.
                 </Button>
-              ))
-            )}
-            {docs.found && docs.found.length === 0 && (
-              <Button variant="text" size="sm" disabled>
-                This user has not created any documents.
-              </Button>
-            )}
-            {!docs.found && (
-              <Button variant="link" size="sm" onClick={() => { fetchCreated('documents'); }}>
-                Click to fetch
-              </Button>
-            )}
-          </td>
-        </tr>
-        <tr>
-          <th>Annotations</th>
-          <td>
-            {annotations.found && annotations.found.length > 0 && (
-              annotations.found.map((annotation) => (
-                <AdminAnnotation annotation={annotation} />
-              ))
-            )}
-            {annotations.found && annotations.found.length === 0 && (
-              <Button variant="text" size="sm" disabled>
-                This user has not created any annotations.
-              </Button>
-            )}
-            {!annotations.found && (
-              <Button variant="link" size="sm" onClick={() => { fetchCreated('annotations'); }}>
-                Click to fetch
-              </Button>
-            )}
-          </td>
-        </tr>
-      </tbody>
-    </Table>
+              )}
+              {!docs.found && (
+                <Button variant="link" size="sm" onClick={() => { fetchCreated('documents'); }}>
+                  Click to fetch
+                </Button>
+              )}
+            </td>
+          </tr>
+          <tr>
+            <th>Annotations</th>
+            <td>
+              {annotations.found && annotations.found.length > 0 && (
+                annotations.found.map((annotation) => (
+                  <AdminAnnotation annotation={annotation} />
+                ))
+              )}
+              {annotations.found && annotations.found.length === 0 && (
+                <Button variant="text" size="sm" disabled>
+                  This user has not created any annotations.
+                </Button>
+              )}
+              {!annotations.found && (
+                <Button variant="link" size="sm" onClick={() => { fetchCreated('annotations'); }}>
+                  Click to fetch
+                </Button>
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </Table>
+      <style jsx global>
+        {`
+          #user-dropdown .dropdown-menu {
+            right: 0 !important;
+            left: auto !important;
+          }
+        `}
+      </style>
+    </>
   );
 };
 
