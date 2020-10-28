ee9632043924ae1ab5e821c7c0392e3945f3bd85
Ben Silverman <bsilverm@mit.edu>
2020-10-28 12:22:30 -0600

    #50 Sorting for admin groups table

--- src/components/Admin/AdminPanel/AdminPanel.js
+++ src/components/Admin/AdminPanel/AdminPanel.js
@@ -1,14 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 import { useState, useEffect } from 'react';
 import {
-  Card, Pagination,
+  Card, Pagination, Table,
 } from 'react-bootstrap';
+import { ArrowDown, ArrowDownUp, ArrowUp } from 'react-bootstrap-icons';
 import AdminDashboard from '../AdminDashboard';
 import AdminUserList from '../User/AdminUserList';
 import AdminDocumentList from '../Document/AdminDocumentList';
 import AdminGroupList from '../Group/AdminGroupList';
 import AdminHeader from '../AdminHeader';
 import { adminGetList } from '../../../utils/adminUtil';
+import LoadingSpinner from '../../LoadingSpinner';
 
 const AdminPanel = ({
   alerts, setAlerts, session, activeKey, setKey,
@@ -17,7 +19,15 @@ const AdminPanel = ({
   const [page, setPage] = useState(1);
   const [listLoading, setListLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [sortState, setSortState] = useState({ field: 'createdAt', direction: 'desc' });
   const perPage = 20;
+
+  const SortIcon = ({ field }) => {
+    if (field === sortState.field) {
+      if (sortState.direction === 'desc') return <ArrowDown />;
+      return <ArrowUp />;
+    } return <ArrowDownUp style={{ fill: 'gray' }} />;
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -26,7 +36,13 @@ const AdminPanel = ({
         setPage(1);
         setTotalPages(1);
         if (activeKey !== 'dashboard') {
-          const params = `?page=${page}&perPage=${perPage}`;
+          const { field, direction } = sortState;
+          let params = '';
+          if (field === 'createdAt') {
+            params = `?page=${page}&perPage=${perPage}&order=${direction}`;
+          } else {
+            params = `?page=${page}&perPage=${perPage}&sort=${field}&order=${direction}`;
+          }
           await adminGetList(activeKey, params)
             .then((results) => {
               setTotalPages(Math.ceil((results.count) / perPage));
@@ -46,7 +62,13 @@ const AdminPanel = ({
         setListLoading(true);
         setTotalPages(1);
         if (activeKey !== 'dashboard') {
-          const params = `?page=${page}&perPage=${perPage}`;
+          const { field, direction } = sortState;
+          let params = '';
+          if (field === 'createdAt') {
+            params = `?page=${page}&perPage=${perPage}&order=${direction}`;
+          } else {
+            params = `?page=${page}&perPage=${perPage}&sort=${field}&order=${direction}`;
+          }
           await adminGetList(activeKey, params)
             .then((results) => {
               setTotalPages(Math.ceil((results.count) / perPage));
@@ -60,45 +82,73 @@ const AdminPanel = ({
     fetchData();
   }, [page]);
 
+  useEffect(() => {
+    async function fetchData() {
+      if (session) {
+        setListLoading(true);
+        setPage(1);
+        if (activeKey !== 'dashboard') {
+          const { field, direction } = sortState;
+          let params = '';
+          if (field === 'createdAt') {
+            params = `?page=${page}&perPage=${perPage}&order=${direction}`;
+          } else {
+            params = `?page=${page}&perPage=${perPage}&sort=${field}&order=${direction}`;
+          }
+          await adminGetList(activeKey, params)
+            .then((results) => {
+              setData(results);
+              setListLoading(false);
+            })
+            .catch((err) => setAlerts([...alerts, { text: err.message, variant: 'danger' }]));
+        }
+      }
+    }
+    fetchData();
+  }, [sortState]);
+
   return (
     <Card>
       <AdminHeader activeKey={activeKey} setKey={setKey} />
       <Card.Body>
+        {listLoading && activeKey !== 'dashboard' && (
+          <LoadingSpinner />
+        )}
         {activeKey === 'dashboard' && (<AdminDashboard />)}
-        {activeKey !== 'dashboard' && (
+        {!listLoading && activeKey !== 'dashboard' && (
           <Pagination style={{ justifyContent: 'center' }}>
             <Pagination.First disabled={page === 1} onClick={() => setPage(1)} />
             <Pagination.Prev disabled={page === 1} onClick={() => setPage(page - 1)} />
             {page !== 1
               && (
-              <>
-                {[...Array(page).keys()].map((i) => (
-                  i + 1 < page && (
-                    <Pagination.Item
-                      key={`page${i + 1}`}
-                      onClick={() => setPage(i + 1)}
-                    >
-                      {i + 1}
-                    </Pagination.Item>
-                  )
-                ))}
-              </>
+                <>
+                  {[...Array(page).keys()].map((i) => (
+                    i + 1 < page && (
+                      <Pagination.Item
+                        key={`page${i + 1}`}
+                        onClick={() => setPage(i + 1)}
+                      >
+                        {i + 1}
+                      </Pagination.Item>
+                    )
+                  ))}
+                </>
               )}
             <Pagination.Item active>{page}</Pagination.Item>
             {totalPages > 1
               && (
-              <>
-                {[...Array(totalPages).keys()].map((i) => (
-                  i + 1 > page && (
-                    <Pagination.Item
-                      key={`page${i + 1}`}
-                      onClick={() => setPage(i + 1)}
-                    >
-                      {i + 1}
-                    </Pagination.Item>
-                  )
-                ))}
-              </>
+                <>
+                  {[...Array(totalPages).keys()].map((i) => (
+                    i + 1 > page && (
+                      <Pagination.Item
+                        key={`page${i + 1}`}
+                        onClick={() => setPage(i + 1)}
+                      >
+                        {i + 1}
+                      </Pagination.Item>
+                    )
+                  ))}
+                </>
               )}
             <Pagination.Next
               disabled={page === totalPages || totalPages === 0}
@@ -110,23 +160,78 @@ const AdminPanel = ({
             />
           </Pagination>
         )}
-        {activeKey === 'users' && (
+        {!listLoading && activeKey === 'users' && (
           <AdminUserList
             users={data.users}
             loading={listLoading}
           />
         )}
-        {activeKey === 'documents' && (
-        <AdminDocumentList
-          documents={data.documents}
-          loading={listLoading}
-        />
-        )}
-        {activeKey === 'groups' && (
-          <AdminGroupList
-            groups={data.groups}
+        {!listLoading && activeKey === 'documents' && (
+          <AdminDocumentList
+            documents={data.documents}
             loading={listLoading}
           />
+        )}
+        {!listLoading && activeKey === 'groups' && data.groups && (
+          <Table
+            striped
+            bordered
+            hover
+            size="sm"
+            variant="light"
+            style={{ borderCollapse: 'unset' }}
+          >
+            <thead>
+              <tr>
+                <th
+                  onClick={() => {
+                    setSortState({
+                      field: 'name',
+                      direction: sortState.direction === 'desc' ? 'asc' : 'desc',
+                    });
+                  }}
+                  style={{ cursor: 'pointer' }}
+                >
+                  Name
+                  {' '}
+                  <SortIcon field="name" />
+                </th>
+                <th
+                  onClick={() => {
+                    setSortState({
+                      field: 'owner',
+                      direction: sortState.direction === 'desc' ? 'asc' : 'desc',
+                    });
+                  }}
+                  style={{ cursor: 'pointer' }}
+                >
+                  Owner
+                  {' '}
+                  <SortIcon field="owner" />
+                </th>
+                <th>
+                  Members
+                </th>
+                <th
+                  onClick={() => {
+                    setSortState({
+                      field: 'createdAt',
+                      direction: sortState.direction === 'desc' ? 'asc' : 'desc',
+                    });
+                  }}
+                  style={{ cursor: 'pointer' }}
+                >
+                  Created
+                  {' '}
+                  <SortIcon field="createdAt" />
+                </th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <AdminGroupList
+              groups={data.groups}
+            />
+          </Table>
         )}
       </Card.Body>
     </Card>
