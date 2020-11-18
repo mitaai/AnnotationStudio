8269fa037f07a6048e29b1d5007bc2318c762a6e
Ben Silverman <bsilverm@mit.edu>
2020-11-18 10:06:26 -0700

    #87 Admin list tests with new props

--- src/components/Admin/Document/AdminDocumentList/AdminDocumentList.test.js
+++ src/components/Admin/Document/AdminDocumentList/AdminDocumentList.test.js
@@ -3,13 +3,16 @@
  */
 
 import { render } from '@testing-library/react';
+import { ArrowUp } from 'react-bootstrap-icons';
 import AdminDocumentList from './AdminDocumentList';
 
 test('renders documents table (empty list)', async () => {
   const { getByTestId } = render(
     <AdminDocumentList
       documents={[]}
-      loading={false}
+      setSortState={jest.fn}
+      sortState={{ field: 'createdAt', direction: 'desc' }}
+      SortIcon={jest.fn().mockReturnValue(<ArrowUp />)}
     />,
   );
   const docsTable = getByTestId('admin-docs-table');
