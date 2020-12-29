7dc4b970fa6f8a8834d92c8c0241e7886848f7fe
Ben Silverman <bsilverm@mit.edu>
2020-12-29 11:13:17 -0500

    #106 Generalize unauthorized language

--- src/components/UnauthorizedCard/UnauthorizedCard.js
+++ src/components/UnauthorizedCard/UnauthorizedCard.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 const UnauthorizedCard = () => (
   <Card>
     <Card.Header><Card.Title>Not authorized</Card.Title></Card.Header>
-    <Card.Body>Please log in to use the application.</Card.Body>
+    <Card.Body>You are not authorized to view this page.</Card.Body>
   </Card>
 );
 
