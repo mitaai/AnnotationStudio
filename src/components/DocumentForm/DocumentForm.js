2b725fc9df336097dfe9a5be5e5f510ed32b8a23
Ben Silverman <bsilverm@mit.edu>
2021-01-05 14:24:13 -0500

    #126 Ability to edit docx/odt uploaded files

--- src/components/DocumentForm/DocumentForm.js
+++ src/components/DocumentForm/DocumentForm.js
@@ -277,14 +277,16 @@ const DocumentForm = ({
       type: DEFAULTS_PARAGRAPH.p.type,
     },
   ];
-  const [slateValue, setSlateValue] = (mode === 'edit' && data && (!data.uploadContentType || data.uploadContentType === ''))
+  const [slateValue, setSlateValue] = (mode === 'edit' && data && (!data.uploadContentType
+    || (!data.uploadContentType.includes('pdf') && !data.uploadContentType.includes('epub'))))
     ? useState(deserializeHTMLToDocument({ plugins, element: txtHtml.body }))
     : useState(slateInitialValue);
 
   let initialValues = {};
 
   if (mode === 'edit' && data) {
-    if (!data.uploadContentType || data.uploadContentType === '') {
+    if (!data.uploadContentType
+      || (!data.uploadContentType.includes('pdf') && !data.uploadContentType.includes('epub'))) {
       initialValues = {
         ...data,
         textSlate: deserializeHTMLToDocument({ plugins, element: txtHtml.body }),
@@ -352,7 +354,9 @@ const DocumentForm = ({
       {(props) => (
         <Form onSubmit={props.handleSubmit} noValidate className="pt-2">
           {(!data
-            || (data && data.state === 'draft' && (!data.uploadContentType || data.uploadContentType === ''))
+            || (data && data.state === 'draft' && (!data.uploadContentType
+              || (!data.uploadContentType.includes('pdf') && !data.uploadContentType.includes('epub')))
+            )
           ) && (
             <Form.Row>
               <Col>
