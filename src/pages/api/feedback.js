6e78c049367a350c4bb3b0bc7652cbf5c9cc8dbd
Ben Silverman <bsilverm@mit.edu>
2021-01-06 21:37:33 -0500

    #127 Implement feedback form Closes #127.

---
+++ src/pages/api/feedback.js
@@ -0,0 +1,28 @@
+import jwt from 'next-auth/jwt';
+import sgMail from '@sendgrid/mail';
+
+const secret = process.env.AUTH_SECRET;
+
+const handler = async (req, res) => {
+  const { method } = req;
+  if (method === 'POST') {
+    const token = await jwt.getToken({ req, secret });
+    if (token && token.exp > 0) {
+      if (req.body.name && req.body.text) {
+        sgMail.setApiKey(process.env.EMAIL_SERVER_PASSWORD);
+        await sgMail
+          .send({
+            to: process.env.SUPPORT_EMAIL,
+            from: process.env.EMAIL_FROM,
+            subject: `[${process.env.SITE_NAME}] Feedback submission`,
+            text: `${req.body.name} writes: ${req.body.text}`,
+            html: `<h5>${req.body.name} writes:</h5> <p>${req.body.text}</p>`,
+          })
+          .catch((error) => res.status(500).end(`Error sending feedback: ${error.message}`));
+        res.status(200).json({ feedback: req.body.text });
+      } else res.status(400).end('Bad request');
+    } else res.status(403).end('Invalid or expired token');
+  } else res.status(405).end(`Method ${method} Not Allowed`);
+};
+
+export default handler;
