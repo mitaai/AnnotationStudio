499ac0a9b170e75e5de5c8802f21bb9364538039
Ben Silverman <bsilverm@mit.edu>
2021-06-13 18:23:38 -0400

    Add diagnostic admin API for annotation deletion

---
+++ src/pages/api/admin/deleteannos.js
@@ -0,0 +1,44 @@
+import { ObjectId } from 'mongodb';
+import jwt from 'next-auth/jwt';
+import { connectToDatabase } from '../../../utils/dbUtil';
+
+const secret = process.env.AUTH_SECRET;
+
+const handler = async (req, res) => {
+  const { method } = req;
+  if (method === 'GET') {
+    const token = await jwt.getToken({ req, secret });
+    if (token && token.exp > 0) {
+      const { db } = await connectToDatabase();
+      const userObj = await db
+        .collection('users')
+        .findOne({ _id: ObjectId(token.id) });
+      const { role } = userObj;
+      if (role === 'admin') {
+        const doc = await db
+          .collection('annotations')
+          .find({}, { projection: { 'target.document.id': 1 } })
+          .toArray();
+        const annos = doc.map((entry) => ({
+          // eslint-disable-next-line no-underscore-dangle
+          _id: entry._id,
+          docId: ObjectId(entry.target.document.id),
+        }));
+        const annosCheckDoc = await Promise.all(annos.map(async (anno) => {
+          let deleteMe = false;
+          const foundDoc = await db
+            .collection('documents')
+            .findOne({ _id: ObjectId(anno.docId) });
+          if (foundDoc === null) {
+            deleteMe = true;
+          }
+          return { ...anno, deleteMe };
+        }));
+        const annosToDelete = annosCheckDoc.filter((anno) => anno.deleteMe === true);
+        res.status(200).json({ found: JSON.parse(JSON.stringify(annosToDelete)) });
+      } else res.status(403).end('Unauthorized');
+    } else res.status(403).end('Invalid or expired token');
+  } else res.status(405).end(`Method ${method} Not Allowed`);
+};
+
+export default handler;
