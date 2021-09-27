e634b44adbe9058b4698faffd05a5f9dafe57c1b
Joshua Mbogo <mbogo@mit.edu>
2021-09-27 18:40:23 -0400

    fixing syntax error

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -350,44 +350,45 @@ export default function Home({
                   // console.log(session);
                 }
                 if (id !== selectedGroupId) {
-                // if a new group is selected the selected document id and
-                // slug should be cleared and set to undefined
-                setSelectedGroupId(id);
-                setSelectedDocumentId();
-                setSelectedDocumentSlug();
-              }
-            }}
-            selectedDocumentId={selectedDocumentId}
-            setSelectedDocumentId={setSelectedDocumentId}
-            selectedDocumentSlug={selectedDocumentSlug}
-            setSelectedDocumentSlug={setSelectedDocumentSlug}
-            documentPermissions={documentPermissions}
-            setDocumentPermissions={setDocumentPermissions}
-          />
-          <DocumentsChannel
-            flex={2}
-            session={statefulSession || session}
-            selectedGroupId={selectedGroupId}
-            setSelectedGroupId={setSelectedGroupId}
-            selectedDocumentId={selectedDocumentId}
-            setSelectedDocumentId={setSelectedDocumentId}
-            selectedDocumentSlug={selectedDocumentSlug}
-            setSelectedDocumentSlug={setSelectedDocumentSlug}
-            documentPermissions={documentPermissions}
-            setDocumentPermissions={setDocumentPermissions}
-            setAlerts={setAlerts}
-            forceUpdate={!!statefulSession}
-          />
-          <AnnotationsChannel
-            flex={2}
-            session={statefulSession || session}
-            setAlerts={setAlerts}
-            slug={selectedDocumentSlug}
-            selectedGroupId={selectedGroupId}
-            selectedDocumentId={selectedDocumentId}
-            selectedDocumentSlug={selectedDocumentSlug}
-            documentPermissions={documentPermissions}
-          />
+                  // if a new group is selected the selected document id and
+                  // slug should be cleared and set to undefined
+                  setSelectedGroupId(id);
+                  setSelectedDocumentId();
+                  setSelectedDocumentSlug();
+                }
+              }}
+              selectedDocumentId={selectedDocumentId}
+              setSelectedDocumentId={setSelectedDocumentId}
+              selectedDocumentSlug={selectedDocumentSlug}
+              setSelectedDocumentSlug={setSelectedDocumentSlug}
+              documentPermissions={documentPermissions}
+              setDocumentPermissions={setDocumentPermissions}
+            />
+            <DocumentsChannel
+              flex={2}
+              session={statefulSession || session}
+              selectedGroupId={selectedGroupId}
+              setSelectedGroupId={setSelectedGroupId}
+              selectedDocumentId={selectedDocumentId}
+              setSelectedDocumentId={setSelectedDocumentId}
+              selectedDocumentSlug={selectedDocumentSlug}
+              setSelectedDocumentSlug={setSelectedDocumentSlug}
+              documentPermissions={documentPermissions}
+              setDocumentPermissions={setDocumentPermissions}
+              setAlerts={setAlerts}
+              forceUpdate={!!statefulSession}
+            />
+            <AnnotationsChannel
+              flex={2}
+              session={statefulSession || session}
+              setAlerts={setAlerts}
+              slug={selectedDocumentSlug}
+              selectedGroupId={selectedGroupId}
+              selectedDocumentId={selectedDocumentId}
+              selectedDocumentSlug={selectedDocumentSlug}
+              documentPermissions={documentPermissions}
+            />
+          </div>
         </div>
       )}
     </Layout>
