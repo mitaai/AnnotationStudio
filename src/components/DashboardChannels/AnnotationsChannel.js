0c2630113819af29b0728c12b0ec1426d51c2fb6
Joshua Mbogo <mbogo@mit.edu>
2021-10-17 16:03:08 -0400

    Update AnnotationsChannel.js

--- src/components/DashboardChannels/AnnotationsChannel.js
+++ src/components/DashboardChannels/AnnotationsChannel.js
@@ -258,7 +258,7 @@ export default function AnnotationsChannel({
     if (appliedFilters.byGroup.length === 0) {
       // this means that we have to filter all annotations to generate the list of annotations
       // that match the filter
-
+      console.log('aa', aa);
       aa.map((a, i) => {
         if (annotationMatchesFilters(a)) {
           filteredAnnos.push(i);
@@ -288,6 +288,7 @@ export default function AnnotationsChannel({
     for (const [groupId, { shared, mine }] of Object.entries(documents)) {
       const permissions = [].concat(shared ? ['shared'] : []).concat(mine ? ['mine'] : []);
       permissions.map((p) => {
+        console.log('documents[groupId][p].docs', documents[groupId][p].docs);
         documents[groupId][p].docs.map(({ _id, title }) => {
           if (!documentIdsIncluded.includes(_id)) {
             // this means that the user has access to this document but not any annotations in this
@@ -401,6 +402,7 @@ export default function AnnotationsChannel({
       byTag: {},
       byDateCreated: { start: undefined, end: undefined, checked: false },
     };
+    console.log('annos2', annos);
     annos.map(({
       creator, created, target: { document: { groups, id: did, title } }, body: { tags },
     }, i) => {
@@ -995,6 +997,7 @@ export default function AnnotationsChannel({
     setListLoading(true);
     fetchSharedAnnotationsOnDocument({ slug, prefetch: false })
       .then((annos) => {
+        console.log('annos', annos);
         const sortedAnnos = annos.sort((a, b) => new Date(b.modified) - new Date(a.modified));
         const a = {
           mine: sortedAnnos
@@ -1138,6 +1141,7 @@ export default function AnnotationsChannel({
       const aids = [];
       if ((appliedFilters.byGroup.length > 0 && appliedFilters.byGroup.includes(gid))
       || appliedFilters.byGroup.length === 0) {
+        console.log('annosIndexes', annosIndexes);
         const aTiles = annosIndexes.map((i) => {
           if (filteredAnnotations.includes(i)) {
             // eslint-disable-next-line no-underscore-dangle
