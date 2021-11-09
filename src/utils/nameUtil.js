463d6bc69433c18159dfef4be7e91145008f6a6b
Joshua Mbogo <mbogo@mit.edu>
2021-11-09 12:00:19 -0500

    Update nameUtil.js

--- src/utils/nameUtil.js
+++ src/utils/nameUtil.js
@@ -1,9 +1,9 @@
 const FullName = (firstName, lastName) => `${firstName} ${lastName}`;
 const FirstNameLastInitial = (fullName) => {
   const splitted = fullName.split(' ');
-  const firstName = splitted[0]
-  const lastInitial = splitted[1] === undefined ? '' : ` ${splitted[1][0]}.`
-  return firstName + lastInitial
+  const firstName = splitted[0];
+  const lastInitial = splitted[1] === undefined ? '' : ` ${splitted[1][0]}.`;
+  return firstName + lastInitial;
 };
 
 export { FullName, FirstNameLastInitial };
